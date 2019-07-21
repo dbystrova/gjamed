740187426f72503a2356bccb318bbfb9ebc0271b
bystrova <bystrova.daria@gmail.com>
2019-07-21 12:57:39 +0200

    no changes

--- Bagues_run_gjam.R
+++ Bagues_run_gjam.R
@@ -1,4 +1,4 @@
-rm(list=ls())
+#rm(list=ls())
 library(repmis)
 library(gjam)
 library(MASS)
@@ -87,15 +87,18 @@ PA_env_df_2<- PA_env_df_1[!zeros_values,]
 #### Keeping PA data with at least one 1/0
 PA_env_df_3<- PA_env_df_2[which(!(rowSums(is.na(PA_env_df_2[,7:131]))==125)),]
 
+PA_non_miss_env<- PA_env_df_2[!apply(is.na(PA_env_df_2[,7:131]),1,any),]
 
 ####Separation test/train
-smp_size <- floor(0.70 * nrow(PA_env_df_3))
+#data<- PA_env_df_3
+data<- PA_non_miss_env
+smp_size <- floor(0.70 * nrow(data))
 ## set the seed to make your partition reproducible
 set.seed(123)
-train_ind <- sample(seq_len(nrow(PA_env_df_3)), size = smp_size)
+train_ind <- sample(seq_len(nrow(data)), size = smp_size)
 
-train <- PA_env_df_3[train_ind, ]
-test <- PA_env_df_3[-train_ind, ]
+train <- data[train_ind, ]
+test <- data[-train_ind, ]
 ## dim(train)  / 3712  131
 ## dim(test)  /  1591  131
 ########################################################################Group numbers
@@ -105,17 +108,18 @@ Species_names_groups<- read.csv("PFG_Bauges_Description_2017.csv", sep="\t")
 
 it<-1000
 burn<-500
-holdout<- sample(seq_len(nrow(train)), size = 200)
+holdout<- sample(seq_len(nrow(train)), size = 100)
 
 ###########GJAM standart model
 y<- train[,7:131]
 xdata<- train[,2:6]
-formula <- as.formula( ~ bio_1_0 + bio_12_0 + bio_19_0 +bio_8_0 +slope )
+formula <- as.formula( ~ bio_1_0+ bio_19_0+bio_8_0+ slope)
 Ydata  <- gjamTrimY(y,10)$y             # at least 10 plots - re-group rare species
+S<- ncol(Ydata)
 rl <- list(r = 5, N = 50)
-ml   <- list(ng = 1000, burnin = 100, typeNames = 'PA', reductList = rl) #change ml
+ml   <- list(ng = 1000, burnin = 100, typeNames = 'PA', reductList = rl, holdoutIndex=holdout) #change ml
 fit<-gjam(formula, xdata = xdata, ydata = Ydata, modelList = ml)
-save(fit,file="models_Bagues_data_OSS/fit.Rda")
+#save(fit,file="models_Bagues_data_OSS/fit.Rda")
 #no Holdout
 #save(fit,file="models_forest_data_OSS/fit.Rda")
 
@@ -187,11 +191,14 @@ sigma_py<-0.25
 funcPY_root<-function(x) {(x/sigma_py)*(prod((x+sigma_py+c(1:S) -1)/(x+c(1:S) -1))-1) - K}
 alpha.PY<-.bisec(funcPY_root,0.0001,100)
 N_eps<-floor(.compute_tau_mean_large_dim(sigma_py,alpha.PY,eps) + 2*.compute_tau_var_large_dim(sigma_py,alpha.PY,eps))
-rl3   <- list(r = r, N = N_eps, sigma_py=sigma_py, alpha=alpha.PY)
+rl3   <- list(r = 5, N = N_eps, sigma_py=sigma_py, alpha=alpha.PY)
 ml3   <- list(ng = it, burnin = burn, typeNames = 'PA', reductList = rl3)
 
 
-fit3 <- .gjam_3(form,xdata,Ydata,ml3)
+fit3 <- .gjam_3(formula,xdata,Ydata,ml3)
+
+
+
 save(fit3,file="models_Bagues_data_OSS/fit3.Rda")
 
 
@@ -215,6 +222,9 @@ for(i in 1:20){
 
 if(sum(alp_sig$is_less_150==T)==0) cat("!! no choice under N=150, need to recheck!!!")
 
+
+
+
 k<-max(which(alp_sig$is_less_150==T)) #max sigma s.t. N<150
 sigma_py<-alp_sig[k,"sigma"]
 alpha.PY<-alp_sig[k,"alpha"]
@@ -228,14 +238,32 @@ alpha.max_val<-5
 sigma_py_max<-0.5
 N_eps<-floor(.compute_tau_mean_large_dim(sigma_py_max,alpha.max_val,eps) + 2*.compute_tau_var_large_dim(sigma_py_max,alpha.max_val,eps))
 
-rl4   <- list(r = 8, N = N_eps,rate=rate,shape=shape,V1=1,ro.disc=ro.disc) #here to modify N
+rl4   <- list(r = 5, N =N_eps,rate=rate,shape=shape,V1=1,ro.disc=ro.disc) #here to modify N
+ml4   <- list(ng = it, burnin = burn, typeNames = 'PA', reductList = rl4)
 
-fit4<-.gjam_4(form, xdata = xdata, ydata = Ydata, modelList = ml4)
+fit4<-.gjam_4(formula, xdata = xdata, ydata = Ydata, modelList = ml4)
+
+
 save(fit4,file="models_Bagues_data_OSS/fit4.Rda")
 
 
 
 
+########################################## Model comparison
+
+
+
+fit$fit$rmspeAll  #2.257202
+fit1$fit$rmspeAll #2.205223
+fit2$fit$rmspeAll #2.209276
+fit3$fit$rmspeAll #2.203517
+fit4$fit$rmspeAll #2.092136
+
+fit$fit$DIC   #2510192
+fit1$fit$DIC  #2496028
+fit2$fit$DIC  #2505273
+fit3$fit$DIC  #2496243
+fit4$fit$DIC  #2460125
 
 
 
@@ -267,6 +295,146 @@ fit2<-.gjam_2(form, xdata = xdata, ydata = treeYdata, modelList = ml2)
 fit3 <- .gjam_3(form,xdata,treeYdata,ml3)
 fit4<-.gjam_4(form, xdata = xdata, ydata = treeYdata, modelList = ml4)
 
+
+fit$fit$rmspeAll  #2.257202
+fit1$fit$rmspeAll #2.205223
+fit2$fit$rmspeAll #2.209276
+fit3$fit$rmspeAll #2.203517
+fit4$fit$rmspeAll #2.092136
+
+fit$fit$DIC   #2510192
+fit1$fit$DIC  #2496028
+fit2$fit$DIC  #2505273
+fit3$fit$DIC  #2496243
+fit4$fit$DIC  #2460125
+
+
+
+
+
+#check that alpha and sigma posteriors
+#gjam1  
+alpha<-mcmc(fit1$chains$alpha.DP_g)
+plot(alpha)
+acfplot(alpha)
+cumuplot(alpha)
+
+#gjam2
+alpha<-mcmc(fit2$chains$alpha.DP_g)
+plot(alpha)
+acfplot(alpha)
+cumuplot(alpha)
+
+##gjam4
+alpha<-mcmc(fit4$chains$alpha.PY_g)
+alpha<-mcmc(fit4$chains$alpha.PY_g[seq(1,length(fit4$chains$alpha.PY_g),by=20)])
+plot(alpha)
+acfplot(alpha)
+cumuplot(alpha)
+
+discount<-mcmc(fit4$chains$discount.PY_g)
+plot(discount)
+acfplot(discount)
+cumuplot(discount)
+
+#check the convergence
+
+#for sigma
+gjam_mc<- mcmc(fit$chains$sgibbs)
+gjam_mc1<- mcmc(fit1$chains$sgibbs) 
+gjam_mc2<- mcmc(fit2$chains$sgibbs) 
+gjam_mc3<- mcmc(fit3$chains$sgibbs) 
+gjam_mc4<- mcmc(fit4$chains$sgibbs) 
+
+
+par(mfrow=c(2,3))
+hist(effectiveSize(gjam_mc), main="ess(sigma) gjam",lwd=2,col=gray(.6),breaks=100)
+hist(effectiveSize(gjam_mc1), main="ess(sigma) gjam1",lwd=2,col=gray(.6),breaks=100)
+hist(effectiveSize(gjam_mc2), main="ess(sigma) gjam2",lwd=2,col=gray(.6),breaks=100)
+hist(effectiveSize(gjam_mc3), main="ess(sigma) gjam3",lwd=2,col=gray(.6),breaks=100)
+hist(effectiveSize(gjam_mc4), main="ess(sigma) gjam4",lwd=2,col=gray(.6),breaks=100)
+
+# for betas
+beta_mcmc<-mcmc(fit$chains$bgibbs)
+beta_mcmc1<-mcmc(fit1$chains$bgibbs)
+beta_mcmc2<-mcmc(fit2$chains$bgibbs)
+beta_mcmc3<-mcmc(fit3$chains$bgibbs)
+beta_mcmc4<-mcmc(fit4$chains$bgibbs)
+
+#nESS
+par(mfrow=c(2,3))
+hist(effectiveSize(beta_mcmc), main="ess(beta) gjam",lwd=2,col=gray(.6))
+hist(effectiveSize(beta_mcmc1), main="ess(beta) gjam1",lwd=2,col=gray(.6))
+hist(effectiveSize(beta_mcmc2), main="ess(beta) gjam2",lwd=2,col=gray(.6))
+hist(effectiveSize(beta_mcmc3), main="ess(beta) gjam3",lwd=2,col=gray(.6))
+hist(effectiveSize(beta_mcmc4), main="ess(beta) gjam4",lwd=2,col=gray(.6))
+
+
+
+#check the traceplots of K
+trace0<-apply(fit$chains$kgibbs,1,function(x) length(unique(x)))
+#trace1<-apply(fit1$chains$kgibbs,1,function(x) length(unique(x)))
+trace2<-apply(fit2$chains$kgibbs,1,function(x) length(unique(x)))
+trace3<-apply(fit3$chains$kgibbs,1,function(x) length(unique(x)))
+trace4<-apply(fit4$chains$kgibbs,1,function(x) length(unique(x)))
+
+table<-data.frame()
+table<-data.frame("trace"=c(trace0,
+                            #trace1,
+                            trace2,trace3,trace4),
+                  "type"=c(rep("0",length(trace0)),
+                           #rep("1",length(trace1)),
+                           rep("2",length(trace2)),rep("3",length(trace3)),rep("4",length(trace4))),
+                  "x"=rep(1:it,4))
+
+
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
+cols = gg_color_hue(4)
+
+#single traceplots - not useful
+# p1<-ggplot(table[which(table$type=="0"),], aes(x=table$x[which(table$type=="0")],y=table$trace[which(table$type=="0")]))+geom_point()
+# p1
+# p2<-ggplot(table[which(table$type=="1"),], aes(x=table$x[which(table$type=="1")],y=table$trace[which(table$type=="1")]))+geom_point()
+# p2
+# p3<-ggplot(table[which(table$type=="2"),], aes(x=table$x[which(table$type=="2")],y=table$trace[which(table$type=="2")]))+geom_point()
+# p3
+# p4<-ggplot(table[which(table$type=="3"),], aes(x=table$x[which(table$type=="3")],y=table$trace[which(table$type=="3")]))+geom_point()
+# p4
+
+# traceplots altogether
+p<-ggplot(table, aes(x=x,y=trace,col=as.factor(type)))+geom_point()+
+  scale_color_manual(name = c(""), values = cols, labels=c("Original model",
+                                                           #"DP with prior on alpha 1",
+                                                           "DP with prior on alpha 2","PY with fixed alpha, sigma","PY with prior on alpha, sigma"))+
+  labs(title="Traceplots of the posterior of the number of clusters")+xlab("iterations")+theme_bw()
+#pdf("plot_forest_data/forest_data_trace_K.pdf")
+p
+#dev.off()
+
+#check the last weight
+pk_chains0_last<- mcmc(fit$chains$pk_g[,ncol(fit$chains$pk_g)])
+plot(pk_chains1_last)
+pk_chains1_last<- mcmc(fit1$chains$pk_g[,ncol(fit1$chains$pk_g)])
+plot(pk_chains1_last)
+pk_chains2_last<- mcmc(fit2$chains$pk_g[,ncol(fit2$chains$pk_g)])
+plot(pk_chains2_last)
+pk_chains3_last<- mcmc(fit3$chains$pk_g[,ncol(fit3$chains$pk_g)])
+plot(pk_chains3_last)
+pk_chains4_last<- mcmc(fit4$chains$pk_g[,ncol(fit4$chains$pk_g)])
+plot(pk_chains4_last)
+
+#Out of sample prediction
+#TJUR coefficient
+
+
+sum((fit$prediction$ypredMu[1:100,]-Ydata[1:100,])^2)/sum(treeYdata[1:100,]^2) #0.5987808
+sum((fit1$prediction$ypredMu[1:100,]-treeYdata[1:100,])^2)/sum(treeYdata[1:100,]^2) #0.5961853
+sum((fit2$prediction$ypredMu[1:100,]-treeYdata[1:100,])^2)/sum(treeYdata[1:100,]^2) #0.5979932
+sum((fit3$prediction$ypredMu[1:100,]-treeYdata[1:100,])^2)/sum(treeYdata[1:100,]^2) #0.5977189
+sum((fit4$prediction$ypredMu[1:100,]-treeYdata[1:100,])^2)/sum(treeYdata[1:100,]^2) #0.5983279
 
 
 
